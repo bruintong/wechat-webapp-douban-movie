fbb46bd91ef72a12fc8c4cbb08aea6da9a4c2f6b
bruintong <bruintong@gmail.com>
2017-02-18 18:58:55 +0800

    (feature) Padding intheaters, comingsoon, selected movie data.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 App({
     globalData: {
-        
+        doubanBase: "https://api.douban.com",
+        inTheaters: "/v2/movie/in_theaters",
+        comingSoon: "/v2/movie/coming_soon",
+        top250: "/v2/movie/top250",
+        weekly: "/v2/movie/weekly",
+        usBox: "/v2/movie/us_box",
+        newMovies: "/v2/movie/new_movies"
     }
 })
--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,8 +1,23 @@
 // /pages/movie/movie.js
+var app = getApp();
+
 Page({
-  data: {},
+  data: {
+    acquiredSelected: false,
+    inTheaters: {},
+    comingSoon: {},
+    top250: {},
+    weekly: {},
+    newMovie: {},
+    usBox: {}
+  },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var inTheatersURL = app.globalData.doubanBase + app.globalData.inTheaters + "?start=0&&count=10";
+    var comingSoonURL = app.globalData.doubanBase + app.globalData.comingSoon + "?start=0&&count=10";
+
+    this.getMovieListData(inTheatersURL, "inTheaters", "影院热映");
+    this.getMovieListData(comingSoonURL, "comingSoon", "即将上映");
   },
   onReady: function () {
     // 页面渲染完成
@@ -15,6 +30,133 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
+  },
+  getMovieListData: function (url, settedKey, categoryTitle) {
+    var that = this;
+    wx.request({
+      url: url,
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "content-type": "json"
+      }, // 设置请求的 header
+      success: function (res) {
+        // success
+        var data = res.data;
+        that.processMovieListData(data, settedKey, categoryTitle);
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  processMovieListData: function (data, settedKey, categoryTitle) {
+    var movies = [];
+    for (let idx in data.subjects) {
+      var subject = data.subjects[idx];
+      var temp = {
+        id: subject.id,
+        title: subject.title,
+        rating: subject.rating,
+        collectCount: subject.collect_count,
+        images: subject.images,
+        subtype: subject.subtype,
+        directors: subject.directors,
+        casts: subject.casts,
+        year: subject.year
+      };
+      movies.push(temp);
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      movies: movies
+    };
+    this.setData(readyData);
+  },
+  handleTouchMove: function (event) {
+    var offsetTop = event.target.offsetTop;
+    console.log("handleTouchMove offsetTop: " + offsetTop);
+    if (offsetTop > 10 && !this.data.acquiredSelected) {
+      var top250URL = app.globalData.doubanBase + app.globalData.top250 + "?start=0&&count=12";
+      console.log("handleTouchMove top250URL: " + top250URL);
+      if (!this.data.acquiredSelected) {
+        var that = this;
+        wx.request({
+          url: top250URL,
+          method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          header: { 'content-type': "json" }, // 设置请求的 header
+          success: function (res) {
+            // success
+            var data = res.data;
+            that.processSelectedListData(data);
+          },
+          fail: function () {
+            // fail
+          },
+          complete: function () {
+            // complete
+          }
+        });
+        var readyData = {};
+        readyData["acquiredSelected"] = {
+          "acquiredSelected": true
+        }
+        this.setData(readyData);
+      }
+    }
+  },
+  processSelectedListData: function (data) {
+    var top250 = [];
+    var weekly = [];
+    var newMovie = [];
+    var usBox = [];
+    for (let idx in data.subjects) {
+      var subject = data.subjects[idx];
+      var temp = {
+        id: subject.id,
+        title: subject.title,
+        rating: subject.rating,
+        collectCount: subject.collect_count,
+        images: subject.images,
+        subtype: subject.subtype,
+        directors: subject.directors,
+        casts: subject.casts,
+        year: subject.year
+      };
+      if (idx < 3) {
+        top250.push(temp);
+
+      } else if (idx < 6) {
+        weekly.push(temp);
+      } else if (idx < 9) {
+        newMovie.push(temp);
+      } else {
+        usBox.push(temp);
+      }
+    }
+    var readyData = {};
+    readyData["top250"] = {
+      categoryTitle: "豆瓣Top250",
+      movies: top250
+    };
+    readyData["weekly"] = {
+      categoryTitle: "本周口碑榜",
+      movies: weekly
+    };
+    readyData["newMovie"] = {
+      categoryTitle: "新片榜",
+      movies: newMovie
+    };
+    readyData["usBox"] = {
+      categoryTitle: "票房榜",
+      movies: usBox
+    };
+
+    this.setData(readyData);
   },
   bindSearchNavigate: function (event) {
     wx.navigateTo({
