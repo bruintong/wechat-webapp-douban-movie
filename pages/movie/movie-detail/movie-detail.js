72695527762115fb9bb9cff9e0fa1840f554a8d1
bruintong <bruintong@gmail.com>
2017-02-18 14:39:55 +0800

    (feature) Advance movie detail static page.

--- pages/movie/movie-detail/movie-detail.js
+++ pages/movie/movie-detail/movie-detail.js
@@ -1,19 +1,24 @@
 // pages/movie/movie-detail/movie-detail.js
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {},
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+  },
+  bindPoster: function (event) {
+    wx.navigateTo({
+      url: '/pages/movie/movie-detail/movie-poster/movie-poster'
+    });
   }
 })
